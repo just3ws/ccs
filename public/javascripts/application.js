8381dd84020765df6ab367258f8b481a7e6d23fa
Sergio Pereira <sergioafp@gmail.com>
2011-01-27 20:10:53 -0600

    worked in a hack to make the hint text not appear to appear when the page loads by setting the default text color for the hints to the background color. this is a horrible hack but it get's us though the day. ideally, convert to putting the hint on the alt for the field and fabricate the element using jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
 	$('form').
-		focusin(function(evt){ $(evt.target).next().show();}).
-		focusout(function(evt){$(evt.target).next().hide();});
+		focusin(function(evt) { 
+      $(evt.target).next().css('color', '#4d0200').show();
+    }).
+		focusout(function(evt) {
+      $(evt.target).next().hide();
+    });
+});
+
+jQuery(document).ready(function() {
+  $('.hint').hide();
 });
