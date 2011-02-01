7bf8544cc7d31062bea60700c093bd91a7b42922
Michael D. Hall <mdh@just3ws.com>
2011-02-01 15:45:12 -0600

    minor reformatting of application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = exception.message
     redirect_to root_url
