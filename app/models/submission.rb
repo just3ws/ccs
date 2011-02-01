67c532749653078e71341288a858424ee5274467
Michael D. Hall <mdh@just3ws.com>
2011-02-01 15:43:13 -0600

    added default scope to the submissions to automatically load in reverse chronological order

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :user, :autosave => true
+  default_scope :order => 'created_at DESC'
 
   validates :user, :presence => true
   validates :title, :presence => true, :length => {:within => 0..64}
