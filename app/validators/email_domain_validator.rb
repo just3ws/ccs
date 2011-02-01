1949b6a248bc224d6aa1c1321a255f5de51da284
Michael D. Hall <mdh@just3ws.com>
2011-02-01 17:56:11 -0600

    changed squish to strip

--- app/validators/email_domain_validator.rb
+++ app/validators/email_domain_validator.rb
@@ -6,7 +6,7 @@ class EmailDomainValidator < ActiveModel::EachValidator
   end
 
   def valid_email_domain?(email)
-    return false if email.squish.blank?
+    return false if email.strip.blank?
     matches = email.match(/\@(.+)/)
     return false if matches.nil? || matches.length < 2
     domain = matches[1]
