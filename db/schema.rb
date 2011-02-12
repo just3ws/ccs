0f7d26945f81d59fc456f114521eb0dd8638096f
Michael D. Hall <mdh@just3ws.com>
2011-02-12 12:11:11 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(:version => 20110203131004) do
     t.string   "status"
   end
 
-  create_table "roles", :force => true do |t|
-    t.string   "title"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "submissions", :force => true do |t|
     t.string   "email"
     t.string   "first_name"
