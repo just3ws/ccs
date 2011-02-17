92e45bd42cdbbf6b052a4d5455172c4e7ab13056
Michael D. Hall <mdh@just3ws.com>
2011-02-17 11:14:42 -0600

    removed asset_resource and formtastic

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Ccs
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(modernizr jquery jquery.validate jquery.placeholder rails application)
+    config.action_view.javascript_expansions[:defaults] = %w(modernizr jquery jquery.validate jquery.placeholder rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
