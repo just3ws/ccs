f31c1d8fa401ffa565ee425c7277076cb12eb4db
Michael D. Hall <mdh@just3ws.com>
2011-02-19 11:41:13 -0600

    added sample heroku config and remove the root devise url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,5 +49,5 @@ Ccs::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = {:host => 'chicagocodecamp.com'}
+  config.action_mailer.default_url_options = {:host => ENV['ROOT_URL']}
 end
