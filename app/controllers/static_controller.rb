7cb7c4bb8078829fd6c1a46dd7e7ccfb93ab3385
Michael D. Hall <mdh@just3ws.com>
2011-03-21 17:33:16 -0500

    if an admin goes to a page that doesn't have any content then provide them the option to set the content.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,9 +5,13 @@ class StaticController < ApplicationController
 
     @content = Content.find_by_permalink(params[:path])
 
-    unless @content 
+    unless @content
       flash[:alert] = "Sorry, couldn't find the page '#{params[:path]}'."
+      if current_user.admin?
+        redirect_to :controller => :content, :action => :new #new_content_path(Content.new(:permalink => params[:path]))
+      else
       redirect_to root_url
+      end
     end
   end
 end
