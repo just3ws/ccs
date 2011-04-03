6d5dfd04418589538785081fc2395f2ed9f639d7
Michael D. Hall <mdh@just3ws.com>
2011-04-03 08:41:44 -0500

    changed sesjas from setting boolean flags on accepted, rsvped, and mailed to using dates with flag methods representing each

--- app/models/sesja.rb
+++ app/models/sesja.rb
@@ -1,6 +1,25 @@
 class Sesja < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, :autosave => true
+  default_scope :order => 'created_at DESC'
   scope :displayable, :conditions => {:accepted => true, :rsvped => true}
+
+  validates :user, :presence => true
+  validates :title, :presence => true, :length => {:within => 0..64}
+  validates :abstract, :presence => true, :length => {:within => 0..1024}
+  validates_inclusion_of :level, :in => 0..3
+
+  def accepted?
+    !!self.accepted_at
+  end
+
+  def rsvped?
+    !!self.rsvped_at
+  end
+
+  def mailed?
+    !!self.mailed_at
+  end
+
 end
 
 # == Schema Information
--- spec/models/sesja_spec.rb
+++ spec/models/sesja_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
 
 describe Sesja do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".accepted?" do
+    it "is not accepted if the date hasn't been set" do
+      s = Sesja.new
+      s.accepted?.should be_false
+    end
+
+    it "is accepted if the date has been set" do
+      s = Sesja.new(:accepted_at => DateTime.current)
+      s.accepted?.should be_true
+    end
+  end
+  describe ".rsvped?" do
+    it "is not rsvped if the date hasn't been set" do
+      s = Sesja.new
+      s.rsvped?.should be_false
+    end
+
+    it "is rsvped if the date has been set" do
+      s = Sesja.new(:rsvped_at => DateTime.current)
+      s.rsvped?.should be_true
+    end
+  end
+
+  describe ".mailed?" do
+    it "is not mailed if the date hasn't been set" do
+      s = Sesja.new
+      s.mailed?.should be_false
+    end
+
+    it "is mailed if the date has been set" do
+      s = Sesja.new(:mailed_at => DateTime.current)
+      s.mailed?.should be_true
+    end
+  end
 end
 
 # == Schema Information
