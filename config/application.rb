1780839ad9875c90cd4ef2e4e3c745ad6742a769
Michael D. Hall <mdh@just3ws.com>
2011-05-11 12:45:14 -0500

    re-added modernizr

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Ccs
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery.validate jquery.placeholder rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery.validate jquery.placeholder modernizr rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
