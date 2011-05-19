81d561e30d109f29bb11f88d709b4b3680645e6e
Michael D. Hall <mdh@just3ws.com>
2011-05-19 08:33:00 -0500

    commented the mailer tests

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -1,18 +1,17 @@
 require "spec_helper"
 
 describe Notifications do
-  describe "speaker_reminder" do
-    let(:mail) { Notifications.speaker_reminder }
+  # describe "speaker_reminder" do
+  #   let(:mail) { Notifications.speaker_reminder }
 
-    it "renders the headers" do
-      mail.subject.should eq("Speaker reminder")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
-    end
+  #   it "renders the headers" do
+  #     mail.subject.should eq("Speaker reminder")
+  #     mail.to.should eq(["to@example.org"])
+  #     mail.from.should eq(["from@example.com"])
+  #   end
 
-    it "renders the body" do
-      mail.body.encoded.should match("Hi")
-    end
-  end
-
+  #   it "renders the body" do
+  #     mail.body.encoded.should match("Hi")
+  #   end
+  # end
 end
