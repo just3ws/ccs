bfe40294ef8dc95e3c295283bb8531a9680aeac4
Michael D. Hall <mdh@just3ws.com>
2012-02-08 09:15:41 -0600

    removed data management from migrations that was causing fresh migration issues due to constraints added recently

--- db/migrate/20110401234616_create_sesjas.rb
+++ db/migrate/20110401234616_create_sesjas.rb
@@ -14,21 +14,21 @@ class CreateSesjas < ActiveRecord::Migration
     end
 
 
-    submissions = Submission.visible.select([:title, :abstract, :level, :keywords, :user_id])
-    puts "converting #{submissions.count} submission(s)" 
-    submissions.find_each do |s|
-      begin
-        if s.user
-          s[:level] ||= 0
-          puts "loading #{s.title}"
-          sesja = Sesja.find_or_initialize_by_title(s.title)
-          sesja.update_attributes(s.attributes.to_hash)
-          sesja.save!
-        end
-      rescue Exception => e
-        puts "unable to save #{e.inspect}"
-      end
-    end
+    #submissions = Submission.visible.select([:title, :abstract, :level, :keywords, :user_id])
+    #puts "converting #{submissions.count} submission(s)"
+    #submissions.find_each do |s|
+      #begin
+        #if s.user
+          #s[:level] ||= 0
+          #puts "loading #{s.title}"
+          #sesja = Sesja.find_or_initialize_by_title(s.title)
+          #sesja.update_attributes(s.attributes.to_hash)
+          #sesja.save!
+        #end
+      #rescue Exception => e
+        #puts "unable to save #{e.inspect}"
+      #end
+    #end
   end
 
   def self.down
--- db/migrate/20110410173502_add_permalink_to_sesja.rb
+++ db/migrate/20110410173502_add_permalink_to_sesja.rb
@@ -2,14 +2,14 @@ class AddPermalinkToSesja < ActiveRecord::Migration
   def self.up
     add_column :sesjas, :permalink, :string
     add_index :sesjas, :permalink
-    add_index :sesjas, :title 
-    Sesja.all.each do |u|
-      u.update_attribute(:permalink, u.title)
-    end
+    add_index :sesjas, :title
+    #Sesja.all.each do |u|
+      #u.update_attribute(:permalink, u.title)
+    #end
   end
 
   def self.down
-    remove_index :sesjas, :column => :title 
+    remove_index :sesjas, :column => :title
     remove_index :sesjas, :column => :permalink
     remove_column :sesjas, :permalink
   end
