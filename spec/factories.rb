f0804de85897447a4f662bf3ae8515ff88f8e50d
Michael D. Hall <mdh@just3ws.com>
2012-03-07 14:08:15 -0600

    fixed factory girl syntax to modern syntax

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,32 +1,34 @@
-Factory.define :user do |f|
-  f.email "a_user@just3ws.com"
-  f.password "secret"
-  f.password_confirmation "secret"
-end
+FactoryGirl.define  do
+  factory :user do |f|
+    f.email "a_user@just3ws.com"
+    f.password "secret"
+    f.password_confirmation "secret"
+  end
 
-Factory.sequence :email do |n|
-  "#{n.ordinalize}_user@just3ws.com"
-end
+  sequence :email do |n|
+    "#{n.ordinalize}_user@just3ws.com"
+  end
 
-Factory.define :attendee, :parent => :user do |f|
-  f.email "attendee@just3ws.com"
-  f.role "attendee"
-end
+  factory :attendee, :parent => :user do |f|
+    f.email "attendee@just3ws.com"
+    f.role "attendee"
+  end
 
-Factory.define :speaker, :parent => :user do |f|
-  f.email "speaker@just3ws.com"
-  f.role "speaker"
-end
+  factory :speaker, :parent => :user do |f|
+    f.email "speaker@just3ws.com"
+    f.role "speaker"
+  end
 
 
-Factory.define :admin, :parent => :user do |f|
-  f.email "admin@just3ws.com"
-  f.role "admin"
-end
+  factory  :admin, :parent => :user do |f|
+    f.email "admin@just3ws.com"
+    f.role "admin"
+  end
 
-Factory.define :session, :class => Sesja do |f|
-  f.title "factory title for session"
-  f.abstract "factory abstract for session"
-  f.level 1
-  f.association :user
+  factory  :session, :class => Sesja do |f|
+    f.title "factory title for session"
+    f.abstract "factory abstract for session"
+    f.level 1
+    f.association :user
+  end
 end
