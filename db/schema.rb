198d16e5aea07fc50437fd5740f42e379a281f5b
Michael D. Hall <mdh@just3ws.com>
2012-04-03 14:10:16 -0500

    whitespace

--- db/schema.rb
+++ db/schema.rb
@@ -216,19 +216,19 @@ ActiveRecord::Schema.define(:version => 20120201112341) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                :default => "",      :null => false
-    t.string   "encrypted_password",                   :default => ""
+    t.string   "email",                                  :default => "",      :null => false
+    t.string   "encrypted_password",                     :default => ""
     t.string   "reset_password_token"
-    t.string   "remember_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        :default => 0
+    t.integer  "sign_in_count",                          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",                                 :default => "guest"
+    t.string   "role",                                   :default => "guest"
     t.integer  "submissions_id"
     t.string   "home_page"
     t.string   "first_name"
@@ -241,10 +241,10 @@ ActiveRecord::Schema.define(:version => 20120201112341) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "invitation_token",     :limit => 20
+    t.string   "invitation_token",       :limit => 20
     t.datetime "invitation_sent_at"
-    t.string   "speakerrate",          :limit => 2048
-    t.string   "twitter",              :limit => 32
+    t.string   "speakerrate",            :limit => 2048
+    t.string   "twitter",                :limit => 32
     t.string   "permalink"
   end
 
