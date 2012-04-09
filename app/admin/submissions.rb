7059b7b42b6d507f79076fe982c0fbbcc37c7989
Michael D. Hall <mdh@just3ws.com>
2012-04-09 17:42:37 -0500

    updated submissions admin page

--- app/admin/submissions.rb
+++ app/admin/submissions.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Submission do
-  
+
+
+  index do
+    column :version_tag
+    column :title
+    column :abstract
+    column "Submitter" do |submission|
+      "#{submission.user.last_name}, #{submission.user.first_name}"
+    end
+    column "Email" do |submission|
+      submission.user.email
+    end
+  end
 end
